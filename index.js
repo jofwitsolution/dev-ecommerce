2fc4a9ac1f2da9b0a6251d3195340e5048b0279c
Faleye Oluwafemi Joseph <faleyeoluwafemi1@gmail.com>
2023-12-18 11:45:12 +0100

    Add routes

--- index.js
+++ index.js
@@ -16,8 +16,17 @@ app.use(cookieParser(process.env.JWT_SECRET));
 
 //  routers
 const authRouter = require("./routes/authRoutes");
+const userRouter = require("./routes/userRoutes");
+
+// middleware
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFound = require("./middleware/not-found");
 
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 const start = async () => {
